100ad6696b344b16f030151d2727e25324363190
beanz <115846234+chat7689@users.noreply.github.com>
2025-11-29 13:18:43 -0800

    Refactor Firebase communication and LCD display

--- arduino/arduino_r4_firebase.ino
+++ arduino/arduino_r4_firebase.ino
@@ -1,65 +1,83 @@
 #include <WiFiS3.h>
-#include <ArduinoHttpClient.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <R4HttpClient.h>
+#include "arduino_secrets.h"  // Your WiFi credentials
 
-// --- WiFi Credentials ---
-const char* ssid = "FBI Surveillance Van";
-const char* password = "wakerthebaker";
+// Initialize LCD with I2C address 0x27, 16 columns, 2 rows
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// --- Firebase Information ---
-const char* firebaseHost = "arduino-r4-wifi-default-rtdb.firebaseio.com";
-String firebasePath = "/test.json";  // the .json is required for REST API
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
 
-WiFiClient wifi;
-HttpClient client = HttpClient(wifi, firebaseHost, 80);
+// Firebase URL
+const char* FIREBASE_URL = "https://arduino-r4-wifi-default-rtdb.firebaseio.com/lcd/text.json";
+
+WiFiSSLClient wifiClient;
+R4HttpClient http; // Using default constructor
+
+String lastText = "";
+unsigned long lastPoll = 0;
+const unsigned long POLL_MS = 2000;
 
 void setup() {
   Serial.begin(115200);
-  delay(2000);
+  delay(500);
 
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Connecting WiFi...");
 
+  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
     Serial.print(".");
-    delay(500);
   }
 
-  Serial.println("\nConnected!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+  IPAddress ip = WiFi.localIP();
+  lcd.clear();
+  lcd.print("IP Address:");
+  lcd.setCursor(0,1);
+  lcd.print(ip);
+  Serial.print("Connected with IP: "); Serial.println(ip);
 
-  sendTestData();
+  http.begin(wifiClient, FIREBASE_URL); // ready to fetch data
 }
 
 void loop() {
-  // You can add code here to send data repeatedly
-  // For now we only send once on startup
-}
+  if (millis() - lastPoll < POLL_MS) return;
+  lastPoll = millis();
 
-// --- Function to send data to Firebase ---
-void sendTestData() {
-  Serial.println("Sending test data to Firebase...");
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, pass);
+    return;
+  }
 
-  client.beginRequest();
-  client.post(firebasePath);
-  client.sendHeader("Content-Type", "application/json");
-  client.sendHeader("Connection", "close");
-  
-  // Change this to any data you want to send
-  String jsonData = "{\"message\":\"hello from Arduino R4!\"}";
-  
-  client.sendHeader("Content-Length", jsonData.length());
-  client.beginBody();
-  client.print(jsonData);
-  client.endRequest();
+  int code = http.GET();
+  if (code != 200) {
+    Serial.print("HTTP GET failed, code: "); Serial.println(code);
+    return;
+  }
 
-  // Read response
-  int status = client.responseStatusCode();
-  String response = client.responseBody();
+  String body = http.getBody();
+  Serial.print("Response: "); Serial.println(body);
 
-  Serial.print("Status code: ");
-  Serial.println(status);
-  
-  Serial.print("Response: ");
-  Serial.println(response);
+  // Expecting JSON string e.g. "Hello"
+  if (body.length() >= 2 && body[0] == '"' && body[body.length()-1] == '"') {
+    String txt = body.substring(1, body.length()-1);
+    if (txt != lastText) {
+      lastText = txt;
+      lcd.clear();
+      if (txt.length() <= 16) {
+        lcd.print(txt);
+      } else {
+        lcd.print(txt.substring(0,16));
+        lcd.setCursor(0,1);
+        lcd.print(txt.substring(16, min((int)txt.length(), 32)));
+      }
+      Serial.print("LCD updated: "); Serial.println(txt);
+    }
+  } else {
+    Serial.println("Unexpected JSON format");
+  }
 }
